5f41f6adf18292d79a4ca85d2411b603f0713cbb
Techbonanza <dev@johnatangao.com>
2025-05-26 02:36:13 +0000

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import { Gallery } from "./components/gallery.component";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="">
-      <div className="flex flex-wrap">{
+      <div className="flex flex-wrap">
+        {
+          posts.map((post) => (
+            <div key={post.id}>{post.name}</div>
+          ))
+        }
         <Gallery />
-      }</div>
+      </div>
     </main>
   );
 }
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,9 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-
-import { env } from "~/env";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+// src/db.ts
+import { drizzle } from "drizzle-orm/vercel-postgres";
+import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
-
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
-if (env.NODE_ENV !== "production") globalForDb.conn = conn;
-
-export const db = drizzle(conn, { schema });
+export const db = drizzle(sql, { schema });
